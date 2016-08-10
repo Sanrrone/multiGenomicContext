bfd2d048e6a2997e71cd5cf9c328e85c7ab5dd06
Sandro Valenzuela <sanrrone@hotmail.com>
2016-08-10 15:17:54 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # multiGenomicContext
+multiGenomicContext is a python + R script that plot the genomic context of a protein on the genome that you want (or many genomes).
+
+
+#Requisites
+* Python >= 2.7 with the module [Biopython](http://biopython.org/wiki/Download)
+* R (tested in 3.3.1), with modules:
+	* ggplot2  
+	* genoPlotR
+
+# Usage
+
+multiGenomicContext have a minimal use:
+	
+	python multiGenomicContext.py -f protein.fasta -l gbklist
+	
+and a complete use:
+
+	python multiGenomicContext.py -f protein.fasta -l gbklist -u 4 -d 4 -e 1e-5 -i 85 -a 75
+	
+where the options are:
+
+* -f: The protein sequence in fasta format (also can be a multifasta of proteins).
+* -l: The list of gbk (if you have one, also put that name in a file)
+* -u: Number of genes to put the genomic context in upstream search (default: 4)
+* -d Number of genes to put the genomic context in downstream search (default: 4)
+* -e E-value for blastp search (default: 1e-5)
+* -i Identity % of the alignment on blastp results to consider the gene exists on the genome (default: 85)
+* -a Alignment length (%) between gene and the match for blastp search to consider the gene "exists" on the genome (default 75)
+
+
+# Notes
+multiGenomicContext search genes on the gbk because the cds are ordered, but this true only for one chromosome assembly, for gbk files where two or more contigs exists, it's show genomic context for the same contig of the gene
---
+++ multiGenomicContext.py
@@ -0,0 +1,311 @@
+from __future__ import with_statement 
+
+# ==============================================================================
+# 						multiGenomicContext
+#
+# Author: Sandro Valenzuela (sandrolvalenzuead@gmail.com) 
+#
+# Please type "python multiGenomicContext.py -h" for usage help
+#
+# ==============================================================================
+
+__author__ = 'Sandro Valenzuela (sandrolvalenzuead@gmail.com)'
+__version__ = '1.0'
+__date__ = '11 August 2016'
+
+import sys, os, re, subprocess, csv, glob
+from operator import itemgetter
+from collections import deque
+from optparse import OptionParser
+#from Bio.Seq import Seq
+#from Bio.SeqRecord import SeqRecord
+#from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio import SeqIO
+
+def printPlotStep(outfilename):
+	print outfilename
+	plotstep=open("plotstep.R", 'w')
+	plotstep.write("""
+rm(list=ls())
+library(ggplot2)
+library(genoPlotR)
+args<-commandArgs()
+outfilename<-args[6]
+temp = list.files(pattern="*.DNASEGcsv")
+if (length(temp)>1) {
+  gbknames<- lapply(as.list(temp),function(x){strsplit(x = gsub(pattern = "[.]",replacement = " ",x = x),split = c(" "))[[1]][1]})
+  
+  df<-lapply(temp, read.csv, header = FALSE)
+  df<-lapply(df,function(x){colnames(x)<-c("name", "start",  "end" ,"strand"  ,"col" ,"lty" ,"lwd" ,"pch" ,"cex", "gene_type");x})
+  df<-lapply(df,function(x){dna_seg(x)})
+  names(df)<-gbknames
+}else{
+  df<-read.csv(temp,header = F)
+  colnames(df)<-c("name", "start",  "end" ,"strand"  ,"col" ,"lty" ,"lwd" ,"pch" ,"cex", "gene_type")
+  df<-list(dna_seg(df))
+}
+
+temp = list.files(pattern="*.ANNOTcsv")
+if (length(temp)>1) {
+  dfa<-lapply(temp, read.csv, header = FALSE)
+  dfa<-lapply(dfa,function(x){colnames(x)<-c("x1" ,  "x2"  ,  "text" ,"color" ,"rot");x})
+  dfa<-lapply(dfa,function(x){annotation(x1=c(as.matrix(x$x1)), x2=NA, text =c(as.matrix(x$text)),
+                                         col = c(as.matrix(x$color)), 
+                                         rot = 40)})
+}else{
+  dfa<-read.csv(temp,header = F)
+  colnames(dfa)<-c("x1" ,  "x2"  ,  "text" ,"color" ,"rot")
+  dfa<-annotation(x1=c(as.matrix(dfa$x1)), x2=NA, text =c(as.matrix(dfa$text)),
+             col = c(as.matrix(dfa$color)), 
+             rot = 40)
+}
+if(length(temp)>5){
+	pdf(file=outfilename, width = (length(temp)*2)-1, height =(length(temp)*2)+2)
+	plot_gene_map(dna_segs = df, annotations = dfa,annotation_height = length(temp)-1,dna_seg_label_cex = 0.9)
+
+}else{
+	pdf(file=outfilename, width = (length(temp)*4)+4, height =(length(temp)*2)+3)
+	plot_gene_map(dna_segs = df, annotations = dfa,annotation_height = 10,dna_seg_label_cex = 0.9)
+
+}
+
+
+dev.off()""")
+
+	plotstep.close()
+
+	subprocess.call(["Rscript", "plotstep.R", str(outfilename)])
+	filenames = glob.glob('*.ANNOTcsv')
+	for filename in filenames:
+ 		os.remove(filename)
+	
+	filenames = glob.glob('*.DNASEGcsv')
+	for filename in filenames:
+		os.remove(filename)
+	
+	os.remove("plotstep.R")
+
+	return None
+
+def foundGenomicContext(gene,faafile,upstream,downstream,GCX): #function to search genomic context
+	#the faa files are with genes in order and formatted >gene|contig|position
+	gene_list = [] #to save up and downstream genes
+	faa_sequences = SeqIO.parse(open(faafile),'fasta')
+
+	for proteins in faa_sequences:
+		name = str(proteins.id)
+		gene_list.append(name)
+
+	#get the position of our gene
+	gene_position=gene_list.index(gene)
+
+	#calculate the total num of genes to print
+	downstream=downstream+upstream
+	#backup gene_position to change the color
+	ourgene_position=gene_position
+	
+	#save the contigname of out gene
+	contigname=str(gene_list[gene_position]).split("|")[2]
+
+	#check if we are close to the begining of the list
+	if (gene_position-upstream)<0:
+		gene_position=0
+		upstream=upstream-gene_position
+	else:
+		gene_position=gene_position-upstream
+
+
+	outname=str(faafile).replace(".faa","")
+	dna_segs=open(str(outname+".DNASEGcsv"),"w")
+	annot=open(str(outname+".ANNOTcsv"),"w")
+
+	while gene_position<len(gene_list) and downstream>=0:
+
+		#only prints genes in the same contig of our gene
+		if str(gene_list[gene_position]).split("|")[2] == contigname:
+			genid=str(gene_list[gene_position]).split("|")[0]
+			name=str(gene_list[gene_position]).split("|")[1]
+			contig=str(gene_list[gene_position]).split("|")[2]
+			pos1=str(gene_list[gene_position]).split("|")[3].split(":")[0]
+			pos2=str(gene_list[gene_position]).split("|")[3].split(":")[1]
+			strand=str(gene_list[gene_position]).split("|")[3]
+			strand=str(strand).split(":")[2].replace("+","1").replace("-","-1")
+
+			if gene_position == ourgene_position:
+				color="red"
+				GCX.write("%s,%s,%s,%s,%s,%s,%s\n" % (faafile,str(genid+"_match"),contig,name,pos1,pos2,strand))
+
+			else:
+				color="gray"
+				GCX.write("%s,%s,%s,%s,%s,%s,%s\n" % (faafile,genid,contig,name,pos1,pos2,strand))
+
+
+			#print name,pos1,pos2,strand,color
+
+			dna_segs.write("%s,%s,%s,%s,%s,1,1,8,1,arrows\n" % (str(name[0:25]+"..."), pos1, pos2, strand, color))
+			annot.write("%s,,%s,%s,35\n" % (str((int(pos2)-int(pos1))/6 + int(pos1)), str(name[0:25]+"..."), color))
+
+			gene_position=gene_position+1
+			downstream=downstream-1
+
+	dna_segs.close()
+	annot.close()
+
+	return None
+
+
+def which(program):#function to check if some program exists 
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def Makefaa(gbk):
+	wd=os.getcwd()
+	gbkname=gbk.replace("/"," ").split()[len(gbk.replace("/"," ").split())-1]
+	protdict={}
+	contiggene={}
+	location={}
+	location2={}
+	location3={}
+	faa= open(str(wd+"/"+gbkname+ ".faa"), 'w')
+	recs = [rec for rec in SeqIO.parse(gbk, "genbank")]
+	for rec in recs:
+		contigname=rec[0:].id
+		feats = [feat for feat in rec.features if feat.type == "CDS"]
+
+		for feat in feats:
+			if "product" in feat.qualifiers:
+				cdsname=str(feat.qualifiers["locus_tag"]).replace("'","").replace("[","").replace("]","")
+				product=str(feat.qualifiers["product"]).replace("'","").replace("[","").replace("]","").replace(" ","_").replace(",",".")
+				locat1=str(feat.location).replace("[","").replace("]","").replace("(",":").replace(")","").split(":")[0]
+				locat2=str(feat.location).replace("[","").replace("]","").replace("(",":").replace(")","").split(":")[1]
+				locat3=str(feat.location).replace("[","").replace("]","").replace("(",":").replace(")","").split(":")[2]
+
+				if "translation" in feat.qualifiers:	
+					translation=str(feat.qualifiers["translation"]).replace("'","").replace("[","").replace("]","")
+					band=0
+					for seq in protdict.items():
+						if seq[1] == translation:
+							band=1
+
+					if band==0:
+						faa.write(">%s|%s|%s|%s\n" % (cdsname, product, contigname, str(locat1)+":"+str(locat2)+":"+locat3))
+						faa.write("%s\n" % (translation))
+	
+	faa.close
+	return str(gbkname + ".faa")
+
+def main():
+
+	parser = OptionParser(usage = "Usage: python multiGenomicContext.py -f protein.fasta -l MYgbklist.txt")
+	parser.add_option("-f","--proteinFasta",dest="fastaProtein",help="default:none. your protein in fasta format to search on the gbk")
+	parser.add_option("-l","--gbklist",dest="gbkList",help="List of the gbk (remember also have the files), also you can give the complete path in the list")
+	parser.add_option("-u","--upstreamGenes",dest="Upstream",help="default:5 number of genes to search upstream on the gbks",default=4)
+	parser.add_option("-d","--downstreamGenes",dest="Downstream",help="default:5 number of genes to search downstream on the gbks",default=4)
+	parser.add_option("-e","--evalue",dest="evalue",help="default:1e-5 e-value for blastp search",default=1e-5)
+	parser.add_option("-i","--identity",dest="Identity",help="default:85 range 1-100 % of identity on the blastp alignment to consider the gene exists on the genome",default=85)
+	parser.add_option("-a","--alignmentLength",dest="alignL",help="default:75 range 1-100 % of aligment length to consider the gene exists on the genome",default=75)
+
+	(options,args) = parser.parse_args()
+
+	Inputprotein = options.fastaProtein
+	gbkList= options.gbkList
+	Upstream = int(options.Upstream)
+	Downstream = int(options.Downstream)
+	Evalue=str(options.evalue)
+	Identity=int(options.Identity)
+	alignL=int(options.alignL)
+
+
+	#check variables
+	if not Inputprotein:
+		print "No input provided, use -h for help"
+		sys.exit()
+
+	if not gbkList:
+		print "No gbk list provided, use -h for help"
+		sys.exit()
+
+	#searching for blastp
+	blastpBIN=which("blastp")
+	if blastpBIN == None:
+		print "No blastp found, install it before continue"
+		sys.exit()
+
+	fasta_sequences = SeqIO.parse(open(Inputprotein),'fasta')
+	gbks = open(gbkList,'r')
+
+#################################################################################
+	#get proteins from gbks
+	faafiles = [] #create list to save .faa
+	for gbk in gbks:
+		gbk=gbk.rstrip()#delete \n character
+		name=Makefaa(gbk) #makefaa return the name of .faa (and create the file)
+		faafiles.append(name)
+
+	gbks.close()
+#################################################################################
+
+	#walk through the fastas
+	for fasta in fasta_sequences:
+		name, sequence = str(fasta.id), str(fasta.seq)
+		#making an individual fasta with the protein
+		tmp=open('tmp.faa','w')
+		tmp.write(">%s\n%s\n" % (name,sequence))
+		tmp.close()
+
+		GCX=open(str(name+".csv"),'w')
+		GCX.write("source,genId,contig,name,start,end,strand\n")
+		for faa in faafiles:
+			subprocess.call([blastpBIN, "-query", "tmp.faa", "-subject", str(faa), "-out", "tmp.out", "-evalue", Evalue, "-outfmt", "10", "-max_target_seqs", "1", "-max_hsps", "1"])
+			
+			#now we check if the results pass the filter to consider the gene "exists" in the genome
+			if os.path.getsize("tmp.out")>0:
+				tmp=open("tmp.out","r")
+				uniquerow=next(csv.reader(tmp))
+				tmp.close()
+				os.remove("tmp.out")
+				#uniquerow[1] is the name of protein that match with our query (header of the fasta to be specific)
+				#uniquerow[2] is identity
+				#uniquerow[3] is alignment coverage (length)
+				if uniquerow[2]>=Identity and (float(uniquerow[3])/len(sequence))>=(alignL/100.0):
+					#if we are here, so, the protein exist in the gbk, the next step is find the genes up and down stream of the gbk
+					#call the function
+
+					foundGenomicContext(uniquerow[1],faa,Upstream,Downstream,GCX)
+
+			else:
+				print "No match found on gbk",str(">"+name),"for",faa
+				if os.path.isfile("tmp.out"):
+					os.remove("tmp.out")
+
+		os.remove("tmp.faa")
+		GCX.close()
+		#call plot step
+		#sys.exit()
+		printPlotStep(str(name+".pdf"))
+
+
+	for faa in faafiles:
+		os.remove(faa)
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+	sys.exit()
